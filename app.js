e566106b0098434fe3a80a844b5a4356dbd4bcfd
Hazem Hamed <hazemhamed@hazemhamed-mbp.corp.dropbox.com>
2020-07-23 13:59:19 -0700

    test5

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+import HelloSign from 'hellosign-embedded';
 var express = require('express');
 var app = express();
 
@@ -7,3 +8,49 @@ app.get('/', function (req, res) {
 app.listen(process.env.PORT ||3000, function () {
   console.log('Example app listening on port 3000!');
 });
+
+
+
+
+$(document).ready(function () {
+    $("#btnRender").click(function btn() {
+      var client_id = $("#client_id").val()
+      var sign_url = $("#sign_url").val()
+      console.log(client_id);
+      console.log(sign_url);
+
+      // V2 -------------------------------------
+      const client = new HelloSign({
+        clientId: client_id,
+        skipDomainVerification: true,
+        debug: true,
+
+
+      });
+      client.open(sign_url, {
+        // allowCancel: true,
+        // testMode: true,
+        // uxVersion: 2,
+        // skipDomainVerification: true,
+        // hideHeader:true,
+        container: document.getElementById('signatureContainer'),
+
+      })
+      // ------------------------------------- 
+      // HelloSign.init(client_id);
+      // HelloSign.open({
+      //   url: sign_url,
+      //   uxVersion: 2,
+      //   allowCancel: false,
+      //   debug: true,
+      //   skipDomainVerification: true,
+      //   container: document.getElementById('signatureContainer'),
+      //   messageListener: function (eventData) {
+      //     if (eventData.event == 'signature_request_signed') {
+      //       console.log('Document signed at HelloSign');
+
+      //     }
+      //   }
+      // });
+    });
+  });
