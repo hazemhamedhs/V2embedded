6eff977080dbfabe452087e3dd7ba1b21ec0c88c
Hazem Hamed <hazemhamed@hazemhamed-mbp.corp.dropbox.com>
2020-07-23 14:54:46 -0700

    test6

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-import HelloSign from 'hellosign-embedded';
+// import HelloSign from 'hellosign-embedded';
+
+
 var express = require('express');
 var app = express();
 
@@ -12,45 +14,46 @@ app.listen(process.env.PORT ||3000, function () {
 
 
 
-$(document).ready(function () {
-    $("#btnRender").click(function btn() {
-      var client_id = $("#client_id").val()
-      var sign_url = $("#sign_url").val()
-      console.log(client_id);
-      console.log(sign_url);
+// $(document).ready(function () {
 
-      // V2 -------------------------------------
-      const client = new HelloSign({
-        clientId: client_id,
-        skipDomainVerification: true,
-        debug: true,
+//     $("#btnRender").click(function btn() {
+//       var client_id = $("#client_id").val()
+//       var sign_url = $("#sign_url").val()
+//       console.log(client_id);
+//       console.log(sign_url);
+
+//       // V2 -------------------------------------
+//       const client = new HelloSign({
+//         clientId: client_id,
+//         skipDomainVerification: true,
+//         debug: true,
 
 
-      });
-      client.open(sign_url, {
-        // allowCancel: true,
-        // testMode: true,
-        // uxVersion: 2,
-        // skipDomainVerification: true,
-        // hideHeader:true,
-        container: document.getElementById('signatureContainer'),
+//       });
+//       client.open(sign_url, {
+//         // allowCancel: true,
+//         // testMode: true,
+//         // uxVersion: 2,
+//         // skipDomainVerification: true,
+//         // hideHeader:true,
+//         container: document.getElementById('signatureContainer'),
 
-      })
-      // ------------------------------------- 
-      // HelloSign.init(client_id);
-      // HelloSign.open({
-      //   url: sign_url,
-      //   uxVersion: 2,
-      //   allowCancel: false,
-      //   debug: true,
-      //   skipDomainVerification: true,
-      //   container: document.getElementById('signatureContainer'),
-      //   messageListener: function (eventData) {
-      //     if (eventData.event == 'signature_request_signed') {
-      //       console.log('Document signed at HelloSign');
+//       })
+//       // ------------------------------------- 
+//       // HelloSign.init(client_id);
+//       // HelloSign.open({
+//       //   url: sign_url,
+//       //   uxVersion: 2,
+//       //   allowCancel: false,
+//       //   debug: true,
+//       //   skipDomainVerification: true,
+//       //   container: document.getElementById('signatureContainer'),
+//       //   messageListener: function (eventData) {
+//       //     if (eventData.event == 'signature_request_signed') {
+//       //       console.log('Document signed at HelloSign');
 
-      //     }
-      //   }
-      // });
-    });
-  });
+//       //     }
+//       //   }
+//       // });
+//     });
+//   });
