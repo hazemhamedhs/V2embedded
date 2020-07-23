f65afc673ebfa351ac05f9293c1bcd82f83b3d49
Hazem Hamed <hazemhamed@hazemhamed-mbp.corp.dropbox.com>
2020-07-23 12:49:48 -0700

    test2

---
+++ app.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+app.get('/', function (req, res) {
+    res.sendFile('/Users/hazemhamed/Desktop/V2embedded/V2embedded.html');
+});
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
